73fe556aabd4cb7b2801a01ea325b883806af4ec
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-03-15 21:03:28 -0400

    created 'sales_ecuador' table and modified 'sales_data' table

--- sql_files/2_create_tables.sql
+++ sql_files/2_create_tables.sql
@@ -3,7 +3,7 @@
 CREATE TABLE public.sales_canada
 (
     transaction_id VARCHAR(50) PRIMARY KEY,
-    date DATE,
+    transaction_date DATE,
     country VARCHAR(25),
     product_id VARCHAR(50),
     product_name VARCHAR(25),
@@ -22,7 +22,26 @@ CREATE TABLE public.sales_canada
 CREATE TABLE public.sales_china
 (
     transaction_id VARCHAR(50) PRIMARY KEY,
-    date DATE,
+    transaction_date DATE,
+    country VARCHAR(25),
+    product_id VARCHAR(50),
+    product_name VARCHAR(25),
+    category VARCHAR(25),
+    price_per_unit FLOAT,
+    quantity_purchased INT,
+    cost_price FLOAT,
+    discount_applied FLOAT,
+    payment_method VARCHAR(25),
+    customer_age_group VARCHAR(25),
+    customer_gender VARCHAR(25),
+    store_location VARCHAR(25),
+    sales_representative VARCHAR(50)
+);
+
+CREATE TABLE public.sales_ecuador
+(
+    transaction_id VARCHAR(50) PRIMARY KEY,
+    transaction_date DATE,
     country VARCHAR(25),
     product_id VARCHAR(50),
     product_name VARCHAR(25),
@@ -41,7 +60,7 @@ CREATE TABLE public.sales_china
 CREATE TABLE public.sales_india
 (
     transaction_id VARCHAR(50) PRIMARY KEY,
-    date DATE,
+    transaction_date DATE,
     country VARCHAR(25),
     product_id VARCHAR(50),
     product_name VARCHAR(25),
@@ -60,7 +79,7 @@ CREATE TABLE public.sales_india
 CREATE TABLE public.sales_nigeria
 (
     transaction_id VARCHAR(50) PRIMARY KEY,
-    date DATE,
+    transaction_date DATE,
     country VARCHAR(25),
     product_id VARCHAR(50),
     product_name VARCHAR(25),
@@ -79,7 +98,7 @@ CREATE TABLE public.sales_nigeria
 CREATE TABLE public.sales_uk
 (
     transaction_id VARCHAR(50) PRIMARY KEY,
-    date DATE,
+    transaction_date DATE,
     country VARCHAR(25),
     product_id VARCHAR(50),
     product_name VARCHAR(25),
@@ -98,7 +117,7 @@ CREATE TABLE public.sales_uk
 CREATE TABLE public.sales_us
 (
     transaction_id VARCHAR(50) PRIMARY KEY,
-    date DATE,
+    transaction_date DATE,
     country VARCHAR(25),
     product_id VARCHAR(50),
     product_name VARCHAR(25),
@@ -117,6 +136,7 @@ CREATE TABLE public.sales_us
 -- set ownership to the tables to `postgres` user
 ALTER TABLE public.sales_canada OWNER to postgres;
 ALTER TABLE public.sales_china OWNER to postgres;
+ALTER TABLE public.sales_ecuador OWNER to postgres;
 ALTER TABLE public.sales_india OWNER to postgres;
 ALTER TABLE public.sales_nigeria OWNER to postgres;
 ALTER TABLE public.sales_uk OWNER to postgres;
@@ -128,6 +148,8 @@ CREATE TABLE public.sales_data AS
 SELECT * FROM sales_canada
 UNION ALL
 SELECT * FROM sales_china
+UNION ALL
+SELECT * FROM sales_ecuador
 UNION ALL
 SELECT * FROM sales_india
 UNION ALL
