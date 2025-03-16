bd41ec7db2c689a1da05fc411c400a5479833b5a
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-03-15 22:51:21 -0400

    data cleaning--making sure no NULL values or duplicates exist in the dataset

--- sql_files/4_data_cleaning.sql
+++ sql_files/4_data_cleaning.sql
@@ -1,9 +1,11 @@
+-- making sure no NULL values or duplicates exist in the dataset
+
 -- search for any NULL values in all the columns
 SELECT *
 FROM sales_data
 WHERE
     transaction_id IS NULL OR
-    date is NULL OR
+    transaction_date is NULL OR
     country is NULL OR
     product_id is NULL OR
     product_name is NULL OR
@@ -17,30 +19,14 @@ WHERE
     customer_gender IS NULL OR
     store_location IS NULL OR
     sales_representative IS NULL;
-
--- 2 rows returned with NULL values: `price_per_unit` and `quantity_purchased`
-
--- update the NULL value for the `quanitity_purchased` column
-UPDATE sales_data
-SET quantity_purchased = 3
-WHERE transaction_id = '00a30472-89a0-4688-9d33-67ea8ccf7aea';
-
--- update the NULL value for the `price_per_unit` column with its average
-UPDATE sales_data
-SET price_per_unit = (
-    SELECT AVG(price_per_unit)
-    FROM sales_data
-    WHERE price_per_unit IS NOT NULL
-)
-WHERE transaction_id = '001898f7-b696-4356-91dc-8f2b73d09c63';
+-- no NULL values returned
 
 
--- check for any data duplications
+-- check for any data duplicates
 SELECT 
     transaction_id,
     COUNT(*)
 FROM sales_data
 GROUP BY transaction_id
 HAVING COUNT(*) > 1;
-
--- no data returned (no duplicates)
+-- no duplicates returned
