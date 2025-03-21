023d08dd820e3e400d0ddd824a9d3af6f9870454
farah-massuh <farahmassuh.dataanalyst@gmail.com>
2025-03-21 17:00:25 -0400

    searched for business insights

--- sql_files/7_business_insights.sql
+++ sql_files/7_business_insights.sql
@@ -11,10 +11,9 @@ ORDER BY total_revenue desc;
 -- top 5 best-selling products for all of 2024
 SELECT
     product_name,
-    country,
     SUM(quantity_purchased) AS total_units_sold
 FROM sales_data
-GROUP BY product_name, country
+GROUP BY product_name
 ORDER BY total_units_sold desc
 LIMIT 5;
 
@@ -27,3 +26,25 @@ FROM sales_data
 GROUP BY sales_representative
 ORDER BY total_revenue DESC
 LIMIT 5;
+
+
+-- top 5 locations based on sales and profit for all of 2024
+SELECT
+    store_location,
+    SUM(total_amount) AS total_sales,
+    SUM(profit) AS total_profit
+FROM sales_data
+GROUP BY store_location
+LIMIT 5;
+
+-- descriptive analysis for total_amount and profit for all 2024
+SELECT
+    MIN(total_amount) AS min_sales_value,
+    MAX(total_amount) AS max_sales_value,
+    AVG(total_amount) AS avg_sales_value,
+    SUM(total_amount) AS total_sales,
+    MIN(profit) AS min_profit,
+    MAX(profit) AS max_profit,
+    AVG(profit) AS avg_profit,
+    SUM(profit) AS total_profit
+FROM sales_data
